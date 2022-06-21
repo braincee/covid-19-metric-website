26683533d0ba5ef6febc9b6860375f89e310e46b
braincee <kwesi938@gmail.com>
2022-06-21 13:31:22 +0100

    nav skeleton

---
+++ src/components/pages/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TopNav = () => {
+    return (  
+        <div>
+            <img src='' />
+            <div>
+                <h3>World At Large</h3>
+                <p>Total Cases</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default TopNav;
